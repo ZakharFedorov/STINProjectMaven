bf624a5570492c5b6bc079c99532ac0ceede20d1
Zakhar_Fedorov <zakhar.fedorov@tul.cz>
2023-03-24 16:21:09 +0100

    Initial commit

---
+++ src/main/java/com/stin/stinprojectmaven/StinProjectMavenApplication.java
@@ -0,0 +1,13 @@
+package com.stin.stinprojectmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StinProjectMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StinProjectMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stin/stinprojectmaven/StinProjectMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stin.stinprojectmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinProjectMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
